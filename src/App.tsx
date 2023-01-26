d3594d36fd06f7bbdc8bb82422f91846fea77602
Mahfod <mahfod.dev@gmail.com>
2023-01-26 15:59:05 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { fetchBeaconApi } from './api/fetchBeaconApi';
+
+import './App.css';
+
+function App() {
+	const [beaconData, setBeaconData] = useState('');
+
+	const onChangeNewData = () => {
+		fetchBeaconApi().then((data) => {
+			setBeaconData(data.pulse.outputValue);
+		});
+	};
+
+	useEffect(() => {
+		fetchBeaconApi().then((data) => {
+			setBeaconData(data.pulse.outputValue);
+		});
+	}, []);
+
+	return (
+		<div className='App'>
+			<h1>{beaconData}</h1>
+
+			<button onClick={onChangeNewData}>Show me the last random value</button>
+		</div>
+	);
+}
+
+export default App;
